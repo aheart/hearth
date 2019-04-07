33a2a2de7899c1ef72fd9fb57100f4735b4a3261
aheart <aheart@users.noreply.github.com>
2019-04-07 21:06:13 +0300

    Add more cpu metrics

--- src/metrics/cpu.rs
+++ src/metrics/cpu.rs
@@ -5,8 +5,14 @@ use serde_derive::Serialize;
 
 #[derive(Default, PartialEq, Debug, Clone, Serialize)]
 pub struct CpuMetrics {
-    cpu_usage: f64,
-    iowait: f64,
+    cpu_usage: f32,
+    user: f32,
+    nice: f32,
+    system: f32,
+    idle: f32,
+    iowait: f32,
+    irq: f32,
+    softirq: f32,
 }
 
 pub struct CpuMetricPlugin {
@@ -32,7 +38,13 @@ impl MetricPlugin for CpuMetricPlugin {
 
         Metrics::Cpu(CpuMetrics {
             cpu_usage: self.cpu.work_percent(),
+            user: self.cpu.user_percent(),
+            nice: self.cpu.nice_percent(),
+            system: self.cpu.system_percent(),
+            idle: self.cpu.idle_percent(),
             iowait: self.cpu.iowait_percent(),
+            irq: self.cpu.irq_percent(),
+            softirq: self.cpu.softirq_percent(),
         })
     }
 
@@ -113,8 +125,32 @@ impl CpuTimes {
         self.user + self.nice + self.system + self.irq + self.softirq + self.steal
     }
 
+    pub fn user(&self) -> u64 {
+        self.user
+    }
+
+    pub fn nice(&self) -> u64 {
+        self.nice
+    }
+
+    pub fn system(&self) -> u64 {
+        self.system
+    }
+
+    pub fn idle(&self) -> u64 {
+        self.idle
+    }
+
     pub fn iowait(&self) -> u64 {
         self.iowait
+    }
+
+    pub fn irq(&self) -> u64 {
+        self.irq
+    }
+
+    pub fn softirq(&self) -> u64 {
+        self.softirq
     }
 
     pub fn total(&self) -> u64 {
@@ -151,32 +187,73 @@ impl CpuTimes {
 #[derive(Default)]
 pub struct Cpu {
     last_cpu_times: CpuTimes,
-    work_percent: f64,
-    iowait_percent: f64,
+    user_percent: f32,
+    nice_percent: f32,
+    system_percent: f32,
+    idle_percent: f32,
+    iowait_percent: f32,
+    irq_percent: f32,
+    softirq_percent: f32,
+    work_percent: f32,
 }
 
 impl Cpu {
-    pub fn work_percent(&self) -> f64 {
+    pub fn work_percent(&self) -> f32 {
         self.work_percent
     }
 
-    pub fn iowait_percent(&self) -> f64 {
+    pub fn user_percent(&self) -> f32 {
+        self.user_percent
+    }
+
+    pub fn nice_percent(&self) -> f32 {
+        self.nice_percent
+    }
+
+    pub fn system_percent(&self) -> f32 {
+        self.system_percent
+    }
+
+    pub fn idle_percent(&self) -> f32 {
+        self.idle_percent
+    }
+
+    pub fn iowait_percent(&self) -> f32 {
         self.iowait_percent
     }
+
+    pub fn irq_percent(&self) -> f32 {
+        self.irq_percent
+    }
+
+    pub fn softirq_percent(&self) -> f32 {
+        self.softirq_percent
+    }
+
 
     pub fn push(&mut self, cpu_times: CpuTimes) {
         let diff = cpu_times.diff(&self.last_cpu_times);
 
-        let total = diff.total() as f64;
-        let work = diff.work() as f64;
-        let iowait = diff.iowait() as f64;
+        let total = diff.total() as f32;
 
         if total == 0.0 {
-            self.work_percent = 0.0;
+            self.user_percent = 0.0;
+            self.nice_percent = 0.0;
+            self.system_percent = 0.0;
+            self.idle_percent = 0.0;
             self.iowait_percent = 0.0;
+            self.irq_percent = 0.0;
+            self.softirq_percent = 0.0;
+            self.work_percent = 0.0;
         } else {
-            self.work_percent = work / total * 100.0;
-            self.iowait_percent = iowait / total * 100.0;
+            self.user_percent = diff.user() as f32 / total * 100.0;
+            self.nice_percent = diff.nice() as f32 / total * 100.0;
+            self.system_percent = diff.system() as f32 / total * 100.0;
+            self.idle_percent = diff.idle() as f32 / total * 100.0;
+            self.iowait_percent = diff.iowait() as f32 / total * 100.0;
+            self.irq_percent = diff.irq() as f32 / total * 100.0;
+            self.softirq_percent = diff.softirq() as f32 / total * 100.0;
+            self.work_percent = diff.work() as f32 / total * 100.0;
         }
 
         self.last_cpu_times = cpu_times;
@@ -191,11 +268,44 @@ mod test {
     fn test_process_data() {
         let raw_data_1 = "cpu  350732 1048 57727 6753933 12435 0 859 0 0 0";
         let raw_data_2 = "cpu  360767 1051 58366 6829700 12458 0 861 0 0 0";
-        assert_parse(raw_data_1, raw_data_2, 12.350090783980386, 0.02659912801119476);
-        assert_parse("", "", 0.0, 0.0);
+        assert_parse(
+            raw_data_1,
+            raw_data_2,
+            12.350090783980386,
+            11.605315199666933,
+            0.003469451479721056,
+            0.738993165180585,
+            87.62331008800842,
+            0.02659912801119476,
+            0.0,
+            0.002312967653147371,
+        );
+        assert_parse(
+            "",
+            "",
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        );
     }
 
-    fn assert_parse(raw_data_1: &str, raw_data_2: &str, cpu_usage: f64, iowait: f64) {
+    fn assert_parse(
+        raw_data_1: &str,
+        raw_data_2: &str,
+        cpu_usage: f32,
+        user: f32,
+        nice: f32,
+        system: f32,
+        idle: f32,
+        iowait: f32,
+        irq: f32,
+        softirq: f32,
+    ) {
         let mut metric_plugin = CpuMetricPlugin::new();
         let now = SystemTime::now();
         metric_plugin.process_data(raw_data_1, &now);
@@ -203,10 +313,16 @@ mod test {
 
         let expected_metrics = Metrics::Cpu(CpuMetrics {
             cpu_usage,
+            user,
+            nice,
+            system,
+            idle,
             iowait,
+            irq,
+            softirq,
         });
 
-        assert_eq!(metrics, expected_metrics);
+        assert_eq!(expected_metrics, metrics);
     }
 }
 
