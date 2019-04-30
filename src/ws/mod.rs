1714f506372a5d1de1ca52e59c377edc48b791eb
aheart <aheart@users.noreply.github.com>
2019-04-30 22:34:46 +0300

    Upgrade to 1.0

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,16 +1,24 @@
 pub mod server;
 pub mod session;
 
-use actix_web::{ws, Error, HttpRequest, HttpResponse};
+use actix::*;
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_web_actors::ws;
 use std::time::Instant;
 
-pub fn ws_route(req: &HttpRequest<session::WsSessionState>) -> Result<HttpResponse, Error> {
+pub fn ws_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<server::WsServer>>,
+) -> Result<HttpResponse, Error> {
     ws::start(
-        req,
         session::WsSession {
             id: 0,
             hb: Instant::now(),
-            ip: req.peer_addr().map(|s| s.to_string()).unwrap_or_else(|| "Unknown IP".to_string())
+            ip: req.peer_addr().map(|s| s.to_string()).unwrap_or_else(|| "Unknown IP".to_string()),
+            addr: srv.get_ref().clone(),
         },
+        &req,
+        stream
     )
 }
